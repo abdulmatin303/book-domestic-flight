e2b56b9ab8ba06ba51d24420720066802fe769fd
amr303 <amr.abdulmatin@gmail.com>
2022-06-14 00:57:06 +0600

    make info card dynamic with dynamic css clss

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,13 +3,13 @@ import flight from '../../assets/images/flight3.png'
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-base-100">
+        <div class="hero min-h-screen px-12">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={flight} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h1 class="text-4xl font-bold">Welcome to ShareTrip! <br /> Find Flights</h1>
                     <p class="py-6">Up to 10% & Add. up to 1,200 Tk Savings on Domestic Flight Bookings. Valid till 12 Jul ‘22.</p>
-                    <button class="btn btn-primary">Booking</button>
+                    <button class="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Booking</button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../Shared/Carousel';
 import Banner from './Banner';
+import Info from './Info';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
            <Banner></Banner>
+           <Info></Info>
         </div>
     );
 };
