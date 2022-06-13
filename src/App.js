241b5d11701df2fbbe0b3f5f40ab9ed52c6539d4
amr303 <amr.abdulmatin@gmail.com>
2022-06-13 17:15:26 +0600

    daisyui to use default navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div>
-      <button class="btn">Button</button>
+     <Navbar></Navbar>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
